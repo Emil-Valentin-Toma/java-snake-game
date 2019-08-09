1f11c80aa36d13ab11cf1052120c45669482f1ee
Emil V. Toma <evtoma@gmail.com>
2019-08-09 23:58:19 +0100

    part 7/20 checked

--- Apple.java
+++ Apple.java
@@ -1,14 +1,19 @@
 package com.codegym.games.snake;
 import com.codegym.engine.cell.*;
 
-public class Apple extends GameObject{
+public class Apple extends GameObject
 
-private static final String APPLE_SIGN = "\uD83C\uDF4E";
-Apple(int x, int y) {
-    super(x, y);
-}
-public void draw(Game game){
-    game.setCellValueEx(x, y, Color.NONE, APPLE_SIGN, Color.DARKRED,75);
-}
+{
+    public boolean isAlive = true;
+
+    private static final String APPLE_SIGN = "\uD83C\uDF4E";
+
+    Apple(int x, int y) {
+        super(x, y);
+    }
+
+    public void draw(Game game){
+        game.setCellValueEx(x, y, Color.NONE, APPLE_SIGN, Color.DARKRED,75);
+    }
 
 }
--- Snake.java
+++ Snake.java
@@ -5,10 +5,13 @@ import com.codegym.engine.cell.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Snake extends GameObject{
+public class Snake extends GameObject
+{
     private static final String HEAD_SIGN = "\uD83D\uDC7E";
     private static final String BODY_SIGN = "\u26AB";
     private  List<GameObject> snakeParts = new ArrayList<>();
+    public boolean isAlive = true;
+    private Direction direction = Direction.LEFT;
 
     public Snake(int x, int y) {
         super(x,y);
@@ -22,12 +25,26 @@ public class Snake extends GameObject{
 
     public void draw(Game game)
     {
-        game.setCellValue(x, y, HEAD_SIGN);
-        game.setCellValue(x + 1, y, BODY_SIGN);
-        game.setCellValue(x + 2, y, BODY_SIGN);
 
+
+        if (!isAlive) {
+            game.setCellValueEx(x, y, Color.NONE, HEAD_SIGN, Color.RED, 75);
+            game.setCellValueEx(x + 1, y, Color.NONE, BODY_SIGN, Color.RED, 75);
+            game.setCellValueEx(x + 2, y, Color.NONE, BODY_SIGN, Color.RED, 75);
+        }
+        else {
+            game.setCellValueEx(x, y, Color.NONE, HEAD_SIGN, Color.BLUE, 75);
+            game.setCellValueEx(x + 1, y, Color.NONE, BODY_SIGN, Color.ALICEBLUE, 75);
+            game.setCellValueEx(x + 2, y, Color.NONE, BODY_SIGN, Color.ALICEBLUE, 75);
+        }
     }
 
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
 
+    public void move(){
+
+    }
 
 }
